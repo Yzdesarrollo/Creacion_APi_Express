c0e5319dd6e3fd508649a5543c729ad6d2b35071
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-17 12:17:40 -0500

    Creando el servidor con los middlewares y un endpoint

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express'); // 1.Importar biblioteca
 const app = express(); // 2.Instanciar a express
 const port = 3000; // 3.puerto de conexion
 
+//5. Middlewards
+app.use(express.urlencoded({extended:false})); // Para datos en text/plain
+app.use(express.json()); // Para datos en formato json
+
+//6. Endpoints
+app.get('/hello', (req,res)=>{
+res.send('Hello world');
+});
+
+// 4. Crear el servidor
 app.listen(port,()=>{
     console.log(`run server OK ${port}`);
 });
